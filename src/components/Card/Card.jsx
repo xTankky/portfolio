ad935109a62b57a25560bb2c64ace0827ca3b608
Jonathan V <jonathanvgr27@gmail.com>
2021-09-05 17:05:04 +0200

    init

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Card
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import App from './App';
+import {Card} from './components'
+
+ReactDOM.render(
+    <Card />,
+  document.getElementById('root')
+);
