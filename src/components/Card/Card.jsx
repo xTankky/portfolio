47bd183453a507c3216a1d9dda6f15c3534a0f96
Jonathan V <jonathanvgr27@gmail.com>
2021-09-06 03:54:35 +0200

    Added Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Card.css'
 
 export default class Card extends React.Component {
     constructor(props) {
@@ -7,9 +8,26 @@ export default class Card extends React.Component {
 
     render() {
         return (
-            <div>
-                Card
+            <div className="Card-Wrapper">
+                <div className="Card-Header">
+                    <div className="Card-Header-Year">
+                        {this.props.year}
+                    </div>
+                    <div className="Card-Header-Title">
+                        {this.props.title}
+                    </div>
+                </div>
+                <hr />
+                <div className="Card-Body">
+                    {this.props.desc}
+                </div>
             </div>
         )
     }
+}
+
+Card.defaultProps = {
+    year: "0000",
+    title: "Title",
+    desc: "Description"
 }
