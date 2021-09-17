94a47a5cf03249b0d1bd16ba453796c6d6844e7d
Jonathan V <jonathanvgr27@gmail.com>
2021-09-17 07:18:50 +0200

    Navbar Done

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Nav.css'
 
-const url = "http://localhost:3000"
+const Links = [
+    ["Portfolio", "/"],
+    ["Plus d'infos", "/more"],
+    ["Cloud", "/owncloud"],
+]
 
-const Nav = (props) => {
+const MyLink = (props) => {
     return (
-        <div className="Nav-Wrapper">
-            <ul className="Nav-List">
-                <li className="Nav-Active"><a href={`${url}`}>Portfolio</a></li>
-                <li><a href={`${url}/more`}>Plus d'infos</a></li>
-                <li><a href={`${url}/owncloud`}>Cloud</a></li>
-            </ul>
-        </div>
+        <li
+            onClick={props.onClick}
+            className={props.active ? "Nav-Active" : null}
+        >
+            <Link to={props.path}>
+                {props.name}
+            </Link>
+        </li>
     )
 }
 
-export default Nav
+
+export default class Nav extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeLinks: [true, false, false]
+        }
+    }
+
+    onClick(i) {
+        const activeLinks = this.state.activeLinks.slice();
+
+        for (let j = 0; j < activeLinks.length; j++) {
+            activeLinks[j] = false;
+        }
+
+        activeLinks[i] = true;
+
+        this.setState({ activeLinks: activeLinks })
+    }
+
+    render() {
+        return (
+            <div className="Nav-Wrapper">
+                <ul className="Nav-List">
+                    {Links.map((link, i) => (
+                        <MyLink
+                            key={`Link-${link[1]}`}
+                            name={link[0]}
+                            path={link[1]}
+                            active={this.state.activeLinks[i]}
+                            onClick={() => this.onClick(i)}
+                        />
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
